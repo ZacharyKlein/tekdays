e3b8846ac20fb059413235ef64d0315e5aa9536d
Zachary Klein <zak@silver-chalice.com>
2009-08-14 13:36:55 -0500

    more css

--- grails-app/taglib/TekDaysTagLib.groovy
+++ grails-app/taglib/TekDaysTagLib.groovy
@@ -100,15 +100,6 @@ def volunteerButton = {attrs ->
         out << "${attrs.width}"
         out << '''" />'''
     }
-
-    def displayFullName = { attrs ->
-        def user = TekUser.findByUsername(attrs.username)
-        if(user.fullNameShow){
-            out << "${user.fullName}"
-        } else {
-            out << "[hidden]"
-        }
-    }
   
 }
 
