1547675a45bd95d52b796eff702cb77978f7471b
Ben Klein <ben@silver-chalice.com>
2009-08-14 12:07:37 -0500

    added default role for users to BootStrap.groovy

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,9 @@
 class BootStrap {
-//START:addTekUser
+
     def init = { servletContext ->
+        if(!Role.findByAuthority("ROLE_USER"))
+            new Role(authority:"ROLE_USER", description:"A regular user.").save()
+
 /*        //START:NewUsers
         new TekUser(fullName: 'John Doe', 
                     userName: 'jdoe',
