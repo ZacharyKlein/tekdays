7ac7b3daaecc2d3a3ba820c0635186aa18c595b8
Ben Klein <ben@silver-chalice.com>
2009-09-25 22:10:10 -0500

    added home controller for passing data to home page

---
+++ grails-app/controllers/HomeController.groovy
@@ -0,0 +1,21 @@
+class HomeController {
+
+    def authenticateService
+
+    def index = {
+        if(authenticateService.isLoggedIn()){
+            def user = authenticateService.userDomain()
+            def volunteerEvents = TekEvent.createCriteria().list{
+                volunteers{
+                    eq('id' , user.id)
+                }
+            }
+            def organizerEvents = TekEvent.findAllByOrganizer(user)
+
+            return [ volunteerEvents: volunteerEvents, organizerEvents: organizerEvents ]
+
+        }
+
+    }
+
+}
