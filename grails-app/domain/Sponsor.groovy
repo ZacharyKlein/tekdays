63f471599637d30ec00188e95c16a863eba29ee2
Old Dave Klein <old_dave@Klein-MacBook-Pro-2.local>
2009-10-10 12:14:01 -0500

    Committing a minor change to Sponsor

--- grails-app/domain/Sponsor.groovy
+++ grails-app/domain/Sponsor.groovy
@@ -8,14 +8,14 @@ class Sponsor {
         name
     }
 
-    static hasMany=[sponsorships:Sponsorship, tags:Tag]
+    static hasMany=[sponsorships:Sponsorship]
 
     static constraints = {
         name(blank:false)
-        website(blank:false)
+        website(blank:true, nullable:true)
+        rep(nullable:false)
         description(nullable:true, maxSize:5000)
         logo(nullable:true, maxSize:1000000)
         sponsorships(nullable:true)
-        tags(nullable:true)
     }
 }
