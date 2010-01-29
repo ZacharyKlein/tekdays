2443d0b2cdd4bd5c7c0e02ad85a9837cec5705f2
Ben Klein <ben@silver-chalice.com>
2010-01-29 13:15:39 -0600

    notifySponsorOfOrganizerApproval() (SponsorshipService.groovy)

--- grails-app/services/SponsorshipService.groovy
+++ grails-app/services/SponsorshipService.groovy
@@ -1,18 +1,19 @@
 class SponsorshipService {
+
 	def mailService
 
     boolean transactional = true
 
-    def sponsorApproval(sponsorship, event, message) {
+    def sponsorApproval(sponsorship, event) {
 		sponsorship.sponsorApproved = true
 		sponsorship.save()
-		notifyOrganizerOfSponsorApproval(sponsorship, event, message)
+		notifyOrganizerOfSponsorApproval(sponsorship, event)
     }
 
-	def organizerApproval(sponsorship, event, message){
+	def organizerApproval(sponsorship, event){
 		sponsorship.organizerApproved = true
 		sponsorship.save()
-		notifySponsorOfOrganizerApproval(sponsorship, event, message)
+		notifySponsorOfOrganizerApproval(sponsorship, event)
 	}
 
 	def requestSponsorship(sponsor, event, message){
@@ -38,11 +39,30 @@ class SponsorshipService {
 
 ${sponsor.name} has offered to sponsor ${event.name}. ${sponsor.rep.profile?.fullName ?: sponsor.rep.username} (${sponsor.name}'s representative) says:
 
-"$message"
+    $message
 
 Click the link to approve this: http://localhost:8080/tekdays/sponsors/approve/${sponsorship.id}
 """
 	    }
+
+	}
+
+
+    def notifySponsorOfOrganizerApproval(sponsorship, event){
+        def sponsor = Sponsor.get(sponsorship?.sponsor.id)
+        def organizer = TekUser.get(event?.organizer.id)
+        mailService.sendMail {
+            to sponsor?.rep.email
+            from "TekDays.com@gmail.com"
+            subject "${organizer?.profile?.fullName ?: organizer?.username} has accepted your offer to sponsor ${event.name}"
+            body """${sponsor?.rep.profile?.fullName ?: sponsor?.rep.username},
+
+${organizer?.profile?.fullName ?: organizer.username} has accepted ${sponsor?.name}'s offer to sponsor ${event?.name}. To see the event's home page, click this link: http://localhost:8080/tekdays/events/${event?.slug}
+"""
+        }
+    }
+
+	def notifyOrganizerOfSponsorApproval(sponsorship, event){
 
 	}
 
