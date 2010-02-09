8aabcd74499202e6ff61d52e48c8f6f3613fc811
Zachary Klein <zak@dell530.KLEINET>
2010-02-09 15:14:55 -0600

    not used yet

--- grails-app/services/BannerUploadService.groovy
+++ grails-app/services/BannerUploadService.groovy
@@ -1,28 +1,54 @@
-class BannerUploadService {
-
-    boolean transactional = true
-
-    def uploadEventBanner(file, id) {
-
-        def bannerFile = file
-        def event = TekEvent.get(id)
-
-        println "the banner is $banner"
-
-        if(event.banner){
-            def oldBanner = new File(event.banner).delete()
-        }
-
-        event.bannerLocation = "web-app/images/banners/${event.name}/${bannerFile.originalFilename}"
-        event.banner = "${bannerFile.originalFilename}"
-
-        def location = new File(event.bannerLocation)
-        if(!location.exists()){
-           location.mkdirs()
-        }
-        bannerFile?.transferTo(location)
-
-    }
-    
-      
-}
+// class BannerUploadService {
+// 
+//     boolean transactional = true
+// 
+//     def uploadEventBanner(file, id) {
+// 
+//         def bannerFile = file
+//         def bannerName = file.originalFilename
+//         def event = TekEvent.get(id)
+// 
+//         println "the banner is $banner"
+// 
+//         if(event.banner){
+//             def oldBanner = new File(event.banner).delete()
+//         }
+// 
+//         event.bannerLocation = "web-app/images/banners/${event.name}/${bannerFile.originalFilename}"
+//         event.banner = "${bannerFile.originalFilename}"
+// 
+//         def location = new File(event.bannerLocation)
+//         if(!location.exists()){
+//            location.mkdirs()
+//         }
+//         bannerFile?.transferTo(location)
+// 
+//     }
+//     
+//     
+//    
+//         if(!loFile.isEmpty()){
+//             def thisIsATest = sponsorInstance.bannerLocation
+// 
+//             if((thisIsATest) && (params.banner)){
+//                 def oldAvatar = new File(thisIsATest).delete()
+//             }
+// 
+//             sponsorInstance.bannerLocation = "images/banners/${sponsorInstance.name}/"
+//             sponsorInstance.banner = fileName
+// 
+//             def bannerTransfer = "${sponsorInstance.bannerLocation}${sponsorInstance.banner}"
+// 
+//             def location = new File(bannerTransfer)
+// 
+//             if(!location.exists()){
+//                location.mkdirs()
+//             }
+// 
+//             loFile?.transferTo(location)
+//         }
+//     
+//     
+//     
+//       
+// }
