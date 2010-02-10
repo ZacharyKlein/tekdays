b01403209f09789b45962392e5f963210b46fb25
Zachary Klein <zak@dell530.KLEINET>
2010-02-10 15:20:15 -0600

    added uploadSponsorbanner method

--- grails-app/services/BannerUploadService.groovy
+++ grails-app/services/BannerUploadService.groovy
@@ -1,54 +1,71 @@
-// class BannerUploadService {
-// 
-//     boolean transactional = true
-// 
-//     def uploadEventBanner(file, id) {
-// 
-//         def bannerFile = file
-//         def bannerName = file.originalFilename
-//         def event = TekEvent.get(id)
-// 
-//         println "the banner is $banner"
-// 
-//         if(event.banner){
-//             def oldBanner = new File(event.banner).delete()
-//         }
-// 
-//         event.bannerLocation = "web-app/images/banners/${event.name}/${bannerFile.originalFilename}"
-//         event.banner = "${bannerFile.originalFilename}"
-// 
-//         def location = new File(event.bannerLocation)
-//         if(!location.exists()){
-//            location.mkdirs()
-//         }
-//         bannerFile?.transferTo(location)
-// 
-//     }
-//     
-//     
-//    
-//         if(!loFile.isEmpty()){
-//             def thisIsATest = sponsorInstance.bannerLocation
-// 
-//             if((thisIsATest) && (params.banner)){
-//                 def oldAvatar = new File(thisIsATest).delete()
-//             }
-// 
-//             sponsorInstance.bannerLocation = "images/banners/${sponsorInstance.name}/"
-//             sponsorInstance.banner = fileName
-// 
-//             def bannerTransfer = "${sponsorInstance.bannerLocation}${sponsorInstance.banner}"
-// 
-//             def location = new File(bannerTransfer)
-// 
-//             if(!location.exists()){
-//                location.mkdirs()
-//             }
-// 
-//             loFile?.transferTo(location)
-//         }
-//     
-//     
-//     
-//       
-// }
+class BannerUploadService {
+
+    boolean transactional = true
+
+    def uploadEventBanner(file, id) {
+
+        def bannerFile = file
+        def bannerName = file.originalFilename
+        def event = TekEvent.get(id)
+
+        println "the banner is $bannerName"
+
+   
+        if(!bannerFile.isEmpty()){
+            def thisIsATest = event.bannerLocation
+
+            if((thisIsATest) && (bannerFile)){
+                def oldBanner = new File(thisIsATest).delete()
+            }
+
+            event.bannerLocation = "images/banners/${event.name}/"
+            event.bannerName = bannerName
+
+            def bannerTransfer = "web-app/${event.bannerLocation}${event.bannerName}"
+
+            def location = new File(bannerTransfer)
+
+            if(!location.exists()){
+               location.mkdirs()
+            }
+
+            bannerFile?.transferTo(location)
+        }
+
+    }
+
+
+    def uploadSponsorBanner(file, id) {
+
+        def bannerFile = file
+        def bannerName = file.originalFilename
+        def sponsor = Sponsor.get(id)
+
+        println "the banner is $bannerName"
+
+
+        if(!bannerFile.isEmpty()){
+            def thisIsATest = sponsor.bannerLocation
+
+            if((thisIsATest) && (bannerFile)){
+                def oldBanner = new File(thisIsATest).delete()
+            }
+
+            sponsor.bannerLocation = "images/banners/${sponsor.name}/"
+            sponsor.bannerName = bannerName
+
+            def bannerTransfer = "web-app/${sponsor.bannerLocation}${sponsor.bannerName}"
+
+            def location = new File(bannerTransfer)
+
+            if(!location.exists()){
+               location.mkdirs()
+            }
+
+            bannerFile?.transferTo(location)
+        }
+
+    }
+    
+
+}
