9c2514158aa607f0a40d84d72a96cd4fbf06a689
Ben Klein <ben@silver-chalice.com>
2010-02-26 17:13:54 -0600

    add relationship service

---
+++ grails-app/services/RelationshipService.groovy
@@ -0,0 +1,43 @@
+class RelationshipService {
+
+    boolean transactional = true
+
+    def userPendingVolunteer(userId) {
+        def user = TekUser.get(userId)
+        def volunteers = Volunteer.findAllByUser(user)
+        def pending
+        if(volunteers){
+            pending = volunteers.findAll { it?.active == false }
+        }
+        return pending
+    }
+
+    def userPendingSponsorOffer(userId) {
+        def user = TekUser.get(userId)
+        def sponsor = Sponsor.findByRep(user)
+        def pending
+        if(sponsor){
+            def sponsorships = Sponsorship.findAllBySponsor(sponsor)
+            pending = sponsorships.findAll { it?.organizerApproved == false }
+        }
+        return pending
+    }
+
+    def userPendingSponsorRequest(userId) {
+        def user = TekUser.get(userId)
+        def sponsorships = Sponsorship.list().findAll { it?.event.organizer.id == user?.id }
+        def pending
+        if(sponsorships){
+            pending = sponsorships.findAll { it?.sponsorApproved == false }
+        }
+        return pending
+    }
+
+    def pendingRelationships(userId) {
+        def pendingVolunteers = userPendingVolunteer(userId)
+        def pendingSponsorOffers = userPendingSponsorOffer(userId)
+        def pendingSponsorRequests = userPendingSponsorRequest(userId)
+        [ 'pendingVolunteers':pendingVolunteers, 'pendingSponsorOffers':pendingSponsorOffers, 'pendingSponsorRequests':pendingSponsorRequests ]
+    }
+}
+
