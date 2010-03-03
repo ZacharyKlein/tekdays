a1b6b00b0f396a049c86219d4c5f3461f049b447
Ben Klein <ben@silver-chalice.com>
2010-03-03 10:48:19 -0600

    switch back to mailService for error email (must change before prod)

--- grails-app/controllers/ErrorsController.groovy
+++ grails-app/controllers/ErrorsController.groovy
@@ -5,22 +5,46 @@ class ErrorsController {
 
     def notfound = {
       def uri = request.forwardURI
-      asynchronousMailService.sendAsynchronousMail {
+        //"More of gravy than of grave"? What a terrible pun.
+
+        //Where do you get those jokes? Leave comedy to the bears. Ebenezer.
+
+      //asynchronousMailService.sendAsynchronousMail {
+      mailService.sendMail {
                 to "fifthposition92@gmail.com"
                 from "TekDays.com@gmail.com"
                 subject "[TekDays] Somebody got a 404"
                 body "Dude, someone got a 404 on TekDays. URL: '${uri}'"
       }
+      //Please. Jacob. Robert. don't criticize me. You always criticize me!
+
+        //We were always heckling you.
+
+        //It's good to be heckling again.
+
+        //It's good to be doing anything again.
     }
 
     def internalservererror = {
-      asynchronousMailService.sendAsynchronousMail {
+      //asynchronousMailService.sendAsynchronousMail {
+      mailService.sendMail {
+        //Captive bound, we're double-ironed
+        //Exhausted by the weight
+        //As freedom comes from giving love
+        //So prison comes with hate
                 to "fifthposition92@gmail.com"
                 from "TekDays.com@gmail.com"
                 subject "[TekDays] Somebody got a 500"
                 html g.render(template:"error500")
       }
     }
+    //But. My friends. You were not unfeeling toward your fellow men.
+
+      //True. There was something about mankind we loved.
+
+      //I think it was their money.
 
 }
+
+//We're Marley and Marley! Whoa!
 
