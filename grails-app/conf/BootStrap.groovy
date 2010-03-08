28f525e7a8e0291a1343ca26fac9e3f707b322fd
Zachary Klein <zak@dell530.KLEINET>
2010-03-08 11:18:08 -0600

    removed default-avatar juggling

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -12,13 +12,6 @@ class BootStrap {
             def role = new Role(authority:"ROLE_ADMIN", description:"A great dude.").save()
             role.addToPeople(ad)
         }
-
-
-      def defaultAvatarTemp = new File("web-app/images/default-avatar.png")
-
-			def defaultAvatar = new File("web-app/images/avatars/default-avatar.png")
-
-			defaultAvatar << defaultAvatarTemp.newInputStream()
     }
     
     def destroy = {
