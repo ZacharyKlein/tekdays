1622d8a5b1747f13ca9a69d0c80454226686405b
Ben Klein <ben@silver-chalice.com>
2010-03-19 13:22:53 -0500

    replace event finding in registration controller

--- grails-app/controllers/RegistrationController.groovy
+++ grails-app/controllers/RegistrationController.groovy
@@ -14,8 +14,7 @@ class RegistrationController {
         list.each{println it.event}
         println "params entering registration list action are: " + params
         params.max = Math.min( params.max ? params.max.toInteger() : 10,  100)
-        def event = TekEvent.findByName(params.name.decodeHyphen())
-        println "the name that we're finding registrations based on is " + params.name
+        def event = TekEvent.findBySlug(params.slug)
         println "here we're going to list the registrations, and the list will have " + Registration.findAllByEvent(event, params)
         [ registrationInstanceList: Registration.findAllByEvent(event, params) ]
     }
@@ -94,7 +93,7 @@ class RegistrationController {
 
     def create = {
         println params
-        def regEvent = TekEvent.findByName( params.name?.decodeHyphen() )
+        def regEvent = TekEvent.findBySlug( params.slug )
         def registrationInstance = new Registration(event:regEvent)
         registrationInstance.properties = params
         def eventId = regEvent.id
