dd0df336c75e88cea9c4590049c1869eefa96c66
Ben Klein <ben@silver-chalice.com>
2010-03-20 05:41:54 -0500

    add filter for task: associated or admin

--- grails-app/conf/SecurityFilters.groovy
+++ grails-app/conf/SecurityFilters.groovy
@@ -184,6 +184,31 @@ class SecurityFilters {
           }
         }
 
+        task(controller:"task", action:"*"){
+          before = {
+            if(authenticateService.userDomain()){
+              def event
+              if(params.id){
+                def task = Task.get(params.id)
+                event = task.event
+              } else {
+                event = TekEvent.findBySlug(params.slug)
+              }
+              def user = authenticateService.userDomain()
+              if(!event.findAssociatedUsers().find{it.id == user.id} && !user.isAdmin()){
+                flash.message = "Access denied."
+                redirect(controller:"home", action:"index")
+                return false
+              }
+              return true
+          } else {
+            flash.message = "Please login.."
+            redirect(controller:"home", action:"index")
+            return false
+            }
+          }
+        }
+
         /*profileChanges(controller:"tekUser", action:"edit") {
 
             before = {
