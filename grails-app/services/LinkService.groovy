1e545231bc046abc0d97e00b28b6f89e39e2dfaf
Zachary Klein <zak@silver-chalice.com>
2010-04-03 17:44:21 -0500

    changed verifyLinks to accept url only

--- grails-app/services/LinkService.groovy
+++ grails-app/services/LinkService.groovy
@@ -2,24 +2,14 @@ class LinkService {
 
     boolean transactional = true
 
-    def verifyLinks(p) {
-            println "p in LinkService.verifyLinks() is " + p
-            println "in LinkService.verifyLinks(), p's class is " + p.class
-            println "p.properties.each are " + p.properties.each
-            def foo = p.website
-	    /*p?.properties.each{
-	        if(it?.key.contains("website") && it?.value) {
-                    println "we found a property with 'website'"
-	            if(!(it?.value?.contains("http://") || it?.value?.contains("ftp://"))) {
-	                return p?."${it?.key}" = "http://${it?.value}"
-	            }
-	        }
-	    }*/
-            if(p?.website){
-                if(!(p.website.contains("http://") || p.website.contains("ftp://"))) {
-	                return p.website = "http://$foo"
-	            }
-            }
+    def verifyLinks(link) {
+      println "link in LinkService.verifyLinks() is " + link
+      println "link LinkService.verifyLinks(), link's class is " + link
+
+      if(!(link.contains("http://") || link.contains("ftp://"))) {
+        return link = "http://$link"
+	    }
+
     }
 
 }
