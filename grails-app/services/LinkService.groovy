6bd2dee533b9fae84b914aee547f6f59887825cf
Ben Klein <ben@silver-chalice.com>
2010-04-03 15:52:39 -0500

    checking for property called website (LinkService.verifyLinks())

--- grails-app/services/LinkService.groovy
+++ grails-app/services/LinkService.groovy
@@ -6,15 +6,19 @@ class LinkService {
             println "p in LinkService.verifyLinks() is " + p
             println "in LinkService.verifyLinks(), p's class is " + p.class
             println "p.properties.each are " + p.properties.each
-	    p?.properties.each{
+	    /*p?.properties.each{
 	        if(it?.key.contains("website") && it?.value) {
                     println "we found a property with 'website'"
 	            if(!(it?.value?.contains("http://") || it?.value?.contains("ftp://"))) {
 	                return p?."${it?.key}" = "http://${it?.value}"
 	            }
 	        }
-	    }
-
+	    }*/
+            if(p?.website){
+                if(!(p.website.contains("http://") || p.website.contains("ftp://"))) {
+	                return p.website = "http://${p.website}"
+	            }
+            }
     }
 
 }
