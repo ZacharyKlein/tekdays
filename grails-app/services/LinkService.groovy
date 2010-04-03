b0679fbf4f66adfd989293ed2723da36e887cba9
Ben Klein <ben@silver-chalice.com>
2010-04-03 15:33:07 -0500

    we needs to have me some pwintlns

--- grails-app/services/LinkService.groovy
+++ grails-app/services/LinkService.groovy
@@ -3,8 +3,11 @@ class LinkService {
     boolean transactional = true
 
     def verifyLinks(p) {
+            println "p in LinkService.verifyLinks is " + p
+            println "p.properties.each are " + p.properties.each
 	    p?.properties.each{
 	        if(it?.key.contains("website") && it?.value) {
+                    println "we found a property with 'website'"
 	            if(!(it?.value?.contains("http://") || it?.value?.contains("ftp://"))) {
 	                return p?."${it?.key}" = "http://${it?.value}"
 	            }
