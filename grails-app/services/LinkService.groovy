82d700871629a55845bc65775876f5530c55a659
Ben Klein <ben@silver-chalice.com>
2010-04-05 09:44:32 -0500

    i can has link? no? den no chek 4 http (LinkService null checking)

--- grails-app/services/LinkService.groovy
+++ grails-app/services/LinkService.groovy
@@ -6,9 +6,11 @@ class LinkService {
       println "link in LinkService.verifyLinks() is " + link
       println "link LinkService.verifyLinks(), link's class is " + link
 
-      if(!(link.contains("http://") || link.contains("ftp://"))) {
-        return link = "http://$link"
-	    }
+      if(link){
+        if(!(link?.contains("http://") || link?.contains("ftp://"))) {
+          return link = "http://$link"
+        }
+      }
 
     }
 
